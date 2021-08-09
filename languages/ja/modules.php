60948b7cbc5860bf03cc94fd344ba98df3281e10
Takahiro Kambe <taca@back-street.net>
2021-08-09 15:15:31 +0900

    Add japanease translation.

---
+++ languages/ja/modules.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * mp_forms extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2015-2016, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       https://github.com/terminal42/contao-mp_forms
+ */
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['mp_form_steps'] = ['MPFormsの段階', '指定した複数ページにフォームの段階を示すナビゲーションを表示'];
---
+++ languages/ja/tl_form.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * mp_forms extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2015-2016, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       https://github.com/terminal42/contao-mp_forms
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_form']['mp_forms_legend'] = '複数ページのフォーム';
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form']['mp_forms_getParam'] = ['GETパラメーター', 'GETに使用するパラメーターを変更できます。'];
