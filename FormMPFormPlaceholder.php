dedfc10b68e128ed415e63d5860f0ad0a6bfae86
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-05-20 13:45:08 +0200

    Fix PHP 8 compatibility

--- FormMPFormPlaceholder.php
+++ FormMPFormPlaceholder.php
@@ -147,8 +147,8 @@ class FormMPFormPlaceholder extends Widget
                 continue;
             }
 
-            $summaryToken[] = sprintf('<div data-ff-name="%s" class="label">%s</div>', htmlspecialchars($k), $v['label']);
-            $summaryToken[] = sprintf('<div data-ff-name="%s" class="value">%s</div>', htmlspecialchars($k), $v['value']);
+            $summaryToken[] = sprintf('<div data-ff-name="%s" class="label">%s</div>', htmlspecialchars($k), $v['label'] ?? '');
+            $summaryToken[] = sprintf('<div data-ff-name="%s" class="value">%s</div>', htmlspecialchars($k), $v['value'] ?? '');
         }
 
         $tokens['mp_forms_summary'] = implode("\n", $summaryToken);
